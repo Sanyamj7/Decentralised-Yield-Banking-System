e658e1ac68cf1e03426cf9d9b360422aa7d3b689
Sanyamj7 <72522205+Sanyamj7@users.noreply.github.com>
2023-01-29 17:06:01 +0530

    Add files via upload

---
+++ build/precache-manifest.16c90353131a0ca410ce40f29fc6aeec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d409001b9d4541807c89",
+    "url": "/static/css/main.3df24ea6.chunk.css"
+  },
+  {
+    "revision": "d409001b9d4541807c89",
+    "url": "/static/js/main.d409001b.chunk.js"
+  },
+  {
+    "revision": "d3bdddbecf11b8f6a1bb",
+    "url": "/static/css/1.bc86d40f.chunk.css"
+  },
+  {
+    "revision": "d3bdddbecf11b8f6a1bb",
+    "url": "/static/js/1.d3bdddbe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30ff2f4b113dea076d6a00586b8c3ef2",
+    "url": "/index.html"
+  }
+];
